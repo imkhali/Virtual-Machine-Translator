8a8a4b866bea0a9db82e049779b0fd7f2ee2bc51
Islam Khalil <imkhalil@gmail.com>
2021-07-19 21:57:36 +1000

    implemented writeArithmetic and refactored writePushPop

--- src/Model/CodeWriter.java
+++ src/Model/CodeWriter.java
@@ -4,16 +4,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.lang.System.getProperty;
 
 public class CodeWriter {
-    private String asmFilePath;
-    private List<String> bufferCommands;
+    public static final String LINE_SEPARATOR = getProperty("line.separator");
+    public static final int THIS = 3;
+    public static final int TEMP = 5;
+    public static int currentLabelIndex = 0;
+    private final String asmFilePath;
+    private final List<String> bufferCommands;
 
     public CodeWriter(String asmFile) {
         this.asmFilePath = asmFile;
@@ -24,22 +24,15 @@ public class CodeWriter {
         return asmFilePath;
     }
 
-    // EFFECTS: write to asmFile the assembly code that implements the given Arithmetic/Logical command
-    public void writeArithmetic(String command) {
-        bufferCommands.add("dummy for now" + getProperty("line.separator"));
-    }
-
-    private void add(Integer arg1, Integer arg2) {
-
-    }
-
     // EFFECTS: write to asmFile the assembly code that implements the given push/pop command
     public void writePushPop(CommandType commandType, String segment, int index) {
         switch (commandType) {
             case C_PUSH:
+                bufferCommands.add("// push " + segment + " " + index);
                 writePush(segment, index);
                 break;
             case C_POP:
+                bufferCommands.add("// pop " + segment + " " + index);
                 writePop(segment, index);
                 break;
             default:
@@ -53,41 +46,25 @@ public class CodeWriter {
                 writePushConstant(index);
                 break;
             case "local":
+                writePushSegment("@LCL", index);
+                break;
             case "argument":
+                writePushSegment("@ARG", index);
+                break;
             case "this":
+                writePushSegment("@THIS", index);
+                break;
             case "that":
-                writePushLocal(segment, index);
+                writePushSegment("@THAT", index);
                 break;
             case "static":
-                writePushStatic(index);
+                writePushOtherSegment("@Foo." + index);
                 break;
             case "temp":
-                writePushTemp(index);
+                writePushOtherSegment("@R" + (index + TEMP));
                 break;
             case "pointer":
-                writePushPointer(index);
-                break;
-            default:
-                throw new RuntimeException("Got wrong command: expected push or pop");
-        }
-    }
-
-    private void writePop(String segment, int index) {
-        switch (segment) {
-            case "local":
-            case "argument":
-            case "this":
-            case "that":
-                writePopLocal(segment, index);
-                break;
-            case "static":
-                writePopStatic(index);
-                break;
-            case "temp":
-                writePopTemp(index);
-                break;
-            case "pointer":
-                writePopPointer(index);
+                writePushOtherSegment("@R" + (index + THIS));
                 break;
             default:
                 throw new RuntimeException("Got wrong command: expected push or pop");
@@ -97,133 +74,228 @@ public class CodeWriter {
     private void writePushConstant(int index) {
         bufferCommands.add("@" + index);
         bufferCommands.add("D=A");
-        bufferCommands.add("@SP");
-        bufferCommands.add("A=M");
-        bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("M=M+1");
+        pushFromD();
     }
 
-    private void writePushLocal(String segment,int index) {
-        Map<String, String> constants = Stream.of(new String[][] {
-                {"local", "LCL"},
-                {"argument", "ARG"},
-                {"this","THIS"},
-                {"that","THAT"},
-        }).collect(Collectors.toMap(data -> data[0], data->data[1]));
-        bufferCommands.add("@" + constants.get(segment));
-        bufferCommands.add("D=A");
+    private void pushFromD() {
+        bufferCommands.add("@SP");
+        bufferCommands.add("M=M+1");
+        bufferCommands.add("A=M-1");
+        bufferCommands.add("M=D");
+    }
+
+    private void writePushSegment(String segmentBaseAddress, int index) {
+        bufferCommands.add("@" + segmentBaseAddress);
+        bufferCommands.add("D=M");
         bufferCommands.add("@" + index);
         bufferCommands.add("A=D+A");
-        bufferCommands.add("A=M");
         bufferCommands.add("D=M");
-        bufferCommands.add("@SP");
-        bufferCommands.add("A=M");
-        bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("M=M+1");
+        pushFromD();
     }
 
-    private void writePopLocal(String segment, int index) {
-        Map<String, String> constants = Stream.of(new String[][] {
-                {"local", "LCL"},
-                {"argument", "ARG"},
-                {"this","THIS"},
-                {"that","THAT"},
-        }).collect(Collectors.toMap(data -> data[0], data->data[1]));
-        bufferCommands.add("@" + constants.get(segment));
-        bufferCommands.add("D=A");
+    private void writePushOtherSegment(String address) {
+        bufferCommands.add(address);
+        bufferCommands.add("D=M");
+        pushFromD();
+    }
+
+    private void writePop(String segment, int index) {
+        switch (segment) {
+            case "local":
+                writePopSegment("@LCL", index);
+                break;
+            case "argument":
+                writePopSegment("@ARG", index);
+                break;
+            case "this":
+                writePopSegment("@THIS", index);
+                break;
+            case "that":
+                writePopSegment("@THAT", index);
+                break;
+            case "static":
+                writePopOtherSegment("@Foo." + index);
+                break;
+            case "temp":
+                writePopOtherSegment("@R" + (index + TEMP));
+                break;
+            case "pointer":
+                writePopOtherSegment("@R" + (index + THIS));
+                break;
+            default:
+                throw new RuntimeException("Got wrong command: expected push or pop");
+        }
+    }
+
+    private void writePopSegment(String segmentBaseAddress, int index) {
+        bufferCommands.add("@" + segmentBaseAddress);
+        bufferCommands.add("D=M");
         bufferCommands.add("@" + index);
         bufferCommands.add("D=D+A");
         bufferCommands.add("@tmp");
         bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("AM=M-1");
-        bufferCommands.add("D=M");
+        popToD();
         bufferCommands.add("@tmp");
-        bufferCommands.add("M=D");
-    }
-
-    private void writePushStatic(int index) {
-        bufferCommands.add("@Foo." + index);
-        bufferCommands.add("D=M");
-        bufferCommands.add("@SP");
         bufferCommands.add("A=M");
         bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("M=M+1");
     }
 
-    private void writePopStatic(int index) {
+    private void writePopOtherSegment(String address) {
+        popToD();
+        bufferCommands.add(address);
+        bufferCommands.add("M=D");
+    }
+
+    private void popToD() {
         bufferCommands.add("@SP");
         bufferCommands.add("AM=M-1");
         bufferCommands.add("D=M");
-        bufferCommands.add("@Foo." + index);
-        bufferCommands.add("M=D");
     }
 
-    private void writePushTemp(int index) {
-        bufferCommands.add("@" + index);
-        bufferCommands.add("D=A");
-        bufferCommands.add("@R5");
-        bufferCommands.add("A=D+A");
-        bufferCommands.add("D=M");
-        bufferCommands.add("@SP");
-        bufferCommands.add("A=M");
-        bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("M=M+1");
-    }
+    // EFFECTS: write to asmFile the assembly code that implements the given Arithmetic/Logical command
+    public void writeArithmetic(String command) {
+        bufferCommands.add("// " + command);
+        switch (command) {
+            case "add":
+                sAdd();
+                break;
+            case "sub":
+                sSub();
+                break;
+            case "neg":
+                sNeg();
+                break;
+            case "and":
+                sAnd();
+                break;
+            case "or":
+                sOr();
+                break;
+            case "not":
+                sNot();
+                break;
+            case "gt":
+                sGt();
+                break;
+            case "lt":
+                sLt();
+                break;
+            case "eq":
+                sEq();
+                break;
+            default:
+                throw new RuntimeException("Got wrong arithmetic function");
 
-    private void writePopTemp(int index) {
-        bufferCommands.add("@" + index);
-        bufferCommands.add("D=A");
-        bufferCommands.add("@R5");
-        bufferCommands.add("D=D+A");
-        bufferCommands.add("@tmp");
-        bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("AM=M-1");
-        bufferCommands.add("D=M");
-        bufferCommands.add("@tmp");
-        bufferCommands.add("M=D");
-    }
-
-    private void writePushPointer(int index) {
-        if (index == 0) {
-            bufferCommands.add("@THIS");
-        } else if (index == 1) {
-            bufferCommands.add("@THAT");
-        } else {
-            throw new RuntimeException("Expected 0 or 1 only, got: " + index);
         }
-        bufferCommands.add("D=M");
-        bufferCommands.add("@SP");
-        bufferCommands.add("A=M");
-        bufferCommands.add("M=D");
-        bufferCommands.add("@SP");
-        bufferCommands.add("M=M+1");
     }
 
-    private void writePopPointer(int index) {
+    private void sAnd() {
         bufferCommands.add("@SP");
         bufferCommands.add("AM=M-1");
         bufferCommands.add("D=M");
-        if (index == 0) {
-            bufferCommands.add("@THIS");
-        } else if (index == 1) {
-            bufferCommands.add("@THAT");
-        } else {
-            throw new RuntimeException("Expected 0 or 1 only, got: " + index);
-        }
-        bufferCommands.add("M=D");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("M=D&M");
+    }
+
+    private void sOr() {
+        bufferCommands.add("@SP");
+        bufferCommands.add("AM=M-1");
+        bufferCommands.add("D=!M");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("M=!M");
+        bufferCommands.add("M=D&M");
+        bufferCommands.add("M=!M");
+    }
+
+    private void sNot() {
+        bufferCommands.add("@SP");
+        bufferCommands.add("A=M-1");
+        bufferCommands.add("M=!M");
+    }
+
+    private void sAdd() {
+        bufferCommands.add("@SP");
+        bufferCommands.add("AM=M-1");
+        bufferCommands.add("D=M");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("M=D+M");
+    }
+
+    private void sSub() {
+        bufferCommands.add("@SP");
+        bufferCommands.add("AM=M-1");
+        bufferCommands.add("D=M");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("M=D-M");
+    }
+
+    private void sNeg() {
+        bufferCommands.add("@SP");
+        bufferCommands.add("A=M-1");
+        bufferCommands.add("M=-M");
+    }
+
+    private void sEq() {
+        String firstLabel = getNextLabel();
+        String secondLabel = getNextLabel();
+        bufferCommands.add("@SP");
+        bufferCommands.add("AM=M-1");
+        bufferCommands.add("D=M");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("D=M-D");
+        bufferCommands.add("@" + firstLabel);
+        bufferCommands.add("D;JEQ");
+        pushFromDTrueOrFalse(firstLabel, secondLabel);
+    }
+
+    private void sLt() {
+        String firstLabel = getNextLabel();
+        String secondLabel = getNextLabel();
+        bufferCommands.add("@SP");
+        bufferCommands.add("AM=M-1");
+        bufferCommands.add("D=M");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("D=M-D");
+        bufferCommands.add("@" + firstLabel);
+        bufferCommands.add("D;JLT");
+        pushFromDTrueOrFalse(firstLabel, secondLabel);
+    }
+
+    private void sGt() {
+        String firstLabel = getNextLabel();
+        String secondLabel = getNextLabel();
+        bufferCommands.add("@SP");
+        bufferCommands.add("AM=M-1");
+        bufferCommands.add("D=M");
+        bufferCommands.add("A=A-1");
+        bufferCommands.add("D=M-D");
+        bufferCommands.add("@" + firstLabel);
+        bufferCommands.add("D;JGT");
+        pushFromDTrueOrFalse(firstLabel, secondLabel);
+    }
+
+    private void pushFromDTrueOrFalse(String firstLabel, String secondLabel) {
+        bufferCommands.add("@SP");                 // branch false
+        bufferCommands.add("A=M-1");
+        bufferCommands.add("M=0");
+        bufferCommands.add("@" + secondLabel);
+        bufferCommands.add("0;JMP");
+        bufferCommands.add("(" + firstLabel + ")"); // branch true
+        bufferCommands.add("@SP");
+        bufferCommands.add("A=M-1");
+        bufferCommands.add("M=-1");
+        bufferCommands.add("(" + secondLabel + ")");
+    }
+
+    private String getNextLabel() {
+        return "Label" + (currentLabelIndex++);
     }
 
     private void writeToAsmFile() {
         try {
             FileWriter asmFile = new FileWriter(this.asmFilePath);
-            for (String asmStatement: this.bufferCommands)
-                asmFile.write(asmStatement + getProperty("line.separator"));
+            for (String asmStatement : this.bufferCommands)
+                asmFile.write(asmStatement + LINE_SEPARATOR);
             asmFile.close();
             System.out.println("Successfully wrote to the file. " + asmFilePath);
         } catch (IOException e) {
